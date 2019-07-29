f8558e4e990d65508044de39862f25cc26df17a7
feven <feven.m.woldu@gmail.com>
2019-07-29 08:36:44 +0300

    included the search_by_imagename classmethod

--- photos/models.py
+++ photos/models.py
@@ -24,12 +24,21 @@ class Image(models.Model):
     Img_caption = models.CharField(max_length =100)
     Likes = models.IntegerField()
     profile = models.ForeignKey(Profile)
+    comments = models.ForeignKey
 
     def __str__(self):
         return self.Img_name
 
     def save_Img(self):
         self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def search_by_imagename(cls,search_term):
+        photos = cls.objects.filter(Img_name__icontains=search_term)
+        return photos
 
 class Comment(models.Model):
     comment = models.CharField(max_length = 200)
