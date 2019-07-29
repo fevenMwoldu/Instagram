25bcb99ea59b45dc3007c24b7700f1689981374d
feven <feven.m.woldu@gmail.com>
2019-07-29 10:43:53 +0300

    uncommented the code that will allow me to run locally

--- instagram/settings.py
+++ instagram/settings.py
@@ -86,17 +86,18 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 
+#uncomment this if u want to run it on heroku
+# DATABASES = {
+#      'default': dj_database_url.config(
+#          default=config('DATABASE_URL')
+#      )
+#  }
 
-DATABASES = {
-     'default': dj_database_url.config(
-         default=config('DATABASE_URL')
-     )
- }
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # DATABASES = {
 #     'default': {
@@ -121,14 +122,14 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 #uncomment this if u want to run it locally
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'instagram',
-#         'USER': 'feven',
-#     'PASSWORD':'123',
-#     }
-# }
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'instagram',
+         'USER': 'feven',
+     'PASSWORD':'123',
+     }
+ }
 
 
 
